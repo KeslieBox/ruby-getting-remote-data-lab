58b38e7c63d38da36c402b9f5efe07bc5475f0e1
KeslieBox <kesliebox@gmail.com>
2021-01-29 18:44:20 -0600

    commit tree f5c12e5686b9cd9e44019caa6a9e8c2368daafc6

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,28 @@
-# Write your code here
+require 'net/http'
+ require 'open-uri'
+ require 'json'
+ require 'pry'
+
+class GetRequester
+# needs to take in a URL on initialization and send an HTTP GET request on command 
+# needs to build a method for dealing with requests that return JSON.
+
+    
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        parse_hash = JSON.parse(self.get_response_body)
+        parse_hash.collect do |k, v|
+        end
+        parse_hash
+    end
+
+end
